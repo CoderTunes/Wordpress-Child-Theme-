5c375f9bca7861c43da4145b8eee4426aae6d3aa
MD. SHAHAB UDDIN <support@devsit.com>
2015-12-27 13:42:17 +0600

    Update funtions.php

--- funtions.php
+++ funtions.php
@@ -1,10 +1,6 @@
 <?php
-add_action( 'after_setup_theme', 'vce_child_theme_setup', 99 );
-function vce_child_theme_setup(){
-    add_action('wp_enqueue_scripts', 'vce_child_load_scripts');
-    
+function wp_child_enqueue_scripts() {
+	wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+	wp_enqueue_style( 'childstyle' );
 }
-function vce_child_load_scripts() {	
-   wp_register_style('vce_child_load_scripts', trailingslashit(get_stylesheet_directory_uri()).'style.css', false, THEME_VERSION, 'screen');	
-   wp_enqueue_style('vce_child_load_scripts');  
-}
+add_action( 'wp_enqueue_scripts', 'wp_child_enqueue_scripts');
